b022f5418533ba32c9be71fc64423bf7d8be78e2
Samhuw8a <Samuel.huwiler@gmx.ch>
2022-12-06 12:53:10 +0100

    func.py update/ copy

--- 3/func.py
+++ 3/func.py
@@ -1,7 +1,6 @@
 import re
-from itertools import chain
-from collections.abc import Callable
-from typing import Union, Any
+from itertools import chain, product
+
 
 # Most of this is shamelessly stolen from Peter Norvig.
 
@@ -10,81 +9,98 @@ inf = float('inf')
 flatten = chain.from_iterable
 
 
-def mapl(f: Callable, iterable)-> list:
-    """gibt die Listenform einer Map aus"""
+def mapl(f, iterable):
     return list(map(f, iterable))
 
-def mapt(f: Callable, iterable)-> tuple:
+def mapt(f, iterable):
     return tuple(map(f, iterable))
 
-def filterl(f: Callable, iterable)-> list:
+def filterl(f, iterable):
     return list(filter(f, iterable))
 
-def read_input(filename, datatype=str, sep:str ='\n')-> list:
-    """Liest die Input Datei aus als 'datatype' und splitet auf den Seperator."""
+
+def parse_multiline_string(s, datatype=str, sep='\n'):
+    return mapl(datatype, s.split(sep))
+
+def read_input(filename, datatype=str, sep='\n'):
     filename = f"{filename:02d}" if isinstance(filename, int) else filename
-    with open(f"{filename}.txt") as f:
-        contents = f.read().strip().split(sep)
-        return mapl(datatype, contents)
+    with open(f"inputs/{filename}.txt") as f:
+        return parse_multiline_string(f.read(), datatype, sep)
 
-def digits(line:str)->tuple :
-    """Gibt alle Digits ein einem String an."""
-    return mapt(int, line)
+def read_input_line(filename, sep=''):
+    filename = f"{filename:02d}" if isinstance(filename, int) else filename
+    with open(f"inputs/{filename}.txt") as f:
+        contents = f.read().strip()
+        return contents if not sep else contents.split(sep)
 
-def integers(text:str, negative:bool=True)-> tuple:
-    """Gibt alle Zahlen in einem String aus. Wenn negative False ist werden alle Zahlen positiv gemacht."""
+
+def digits(line):
+    return mapl(int, line)
+
+def integers(text, negative=True):
     return mapt(int, re.findall(r"-?\d+" if negative else r"\d+", text))
 
-def count(iterable, pred=bool)-> int:
-    """Zählt alle Zahlen zusammen die die 'pred' bevolgen."""
+
+def count_if(iterable, pred=bool):
     return sum(1 for item in iterable if pred(item))
 
-def first(iterable, default=None)-> Any:
-    """Gibt das erste Element eines iterable an."""
+def first(iterable, default=None):
     return next(iter(iterable), default)
 
-def filter_first(iterable, func: Callable):
-    return first(el for el in iterable if func(el))
+def filter_first(iterable, pred):
+    return first(el for el in iterable if pred(el))
 
-def manhattan(a:tuple , b:tuple = (0, 0) ) -> int:
-    """Manhattan Distanz zwischen zwei punkten a,b (Minecraft ähnlich)"""
+
+def manhattan(a, b=(0, 0)):
     return sum(abs(p - q) for p, q in zip(a, b))
 
-def sign(n:Union[int,float]):
-    """Gibt das 'sign' einer Zahl an."""
+
+def sign(n):
     if n > 0: return 1
     elif n < 0: return -1
     else: return 0
 
-def line_print(lines: str):
-    """Druckt alle Linien in 'lines' Formatiert aus"""
+
+def print_2d(lines):
     for line in lines:
         print(cat(line))
 
-def maxval(d:dict):
-    """Gib das Grösste Value eine Dictionarys an."""
+
+def maxval(d):
     return max(d.values())
 
-def transpose(matrix:Union[list,tuple]):
-    """'Dreht' eine Matrix um."""
+
+def transpose(matrix):
     return list(zip(*matrix))
 
-def bin2int(s:str):
-    """Convertiert eine Binärzahl in eine Decimalzahl."""
+
+def bin2int(s):
     return int(s, 2)
 
-if __name__ == "__main__":
-    assert cat(["ab", "cd", "ef"]) == "abcdef"
-    assert mapl(int, ["1", "2"]) == [1, 2]
-    assert mapt(int, ["1", "2"]) == (1, 2)
-    assert filterl(lambda x: x > 3, [1, 5, 2, 4, 3]) == [5, 4]
-    assert digits("123") == (1, 2, 3)
-    assert integers("23 -42 55") == (23, -42, 55)
-    assert integers("23 -42 55", negative=False) == (23, 42, 55)
-    assert count([3, -5, 10, -7, 33], lambda x: x > 0) == 3
-    assert first([2, 4, 6, 8]) == 2
-    assert filter_first([2, 7, 4, 6, 8], lambda x: x > 5) == 7
-    assert manhattan((5, -3)) == 8
-    assert manhattan((5, -3), (2, 7)) == 13
-    assert maxval(dict(a=3, b=99, c=66)) == 99
-    assert bin2int("0101") == 5
+
+def neighbours(x, y, amount=4):
+    assert amount in {4, 8, 9}
+    for dy, dx in product((-1, 0, 1), repeat=2):
+        if ((amount == 4 and abs(dx) != abs(dy)) or
+            (amount == 8 and not dx == dy == 0) or
+             amount == 9):
+            yield (x+dx, y+dy)
+
+
+def list2grid(lines, pred=None):
+    return {(x, y): val
+            for y, line in enumerate(lines)
+            for x, val in enumerate(line)
+            if (pred(val) if pred else True)}
+
+def grid2list(grid, pred=bool):
+    min_x, min_y = map(min, zip(*grid))
+    if min_x < 0 or min_y < 0:
+        raise ValueError
+    max_x, max_y = map(max, zip(*grid))
+    lines = [[' ' for _ in range(max_x+1)]
+                  for _ in range(max_y+1)]
+    for x, y in grid:
+        if pred(grid[(x, y)]):
+            lines[y][x] = '█'
+    return lines
