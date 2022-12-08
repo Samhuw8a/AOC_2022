9e9f4ed9e4656cbac8c83d8f9d25c4d15e70214e
Samhuw8a <77882404+Samhuw8a@users.noreply.github.com>
2022-12-08 21:33:54 +0100

    Day 7

--- 7/part1.py
+++ 7/part1.py
@@ -1,8 +1,25 @@
 from func import *
+from collections import defaultdict
+from itertools import accumulate
+
 
 inp = read_input("input")
-inp = read_input("test_input")
+#  inp = read_input("test_input")
 
-for i in inp:
-    print(i)
+def parse_input(data:list)->dict:
+    sizes:defaultdict=defaultdict(int)
+    for line in data:
+        match line.split():
+            case "$", "cd", "/"  : path=["/"]
+            case "$", "cd", ".." : path.pop()
+            case "$", "cd", f    : path.append(f+"/")
+            case "$", "ls"       : pass
+            case  "dir",_        : pass
+            case val, _:
+                for p in accumulate(path):
+                    sizes[p] += int(val)
+    return sizes
 
+sizes= parse_input(inp)
+tot= sum(v for v in sizes.values() if v<=100_000)
+print(tot)
